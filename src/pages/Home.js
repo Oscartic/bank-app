c3b4ecddd91a4b03bd642e2ea3f8e14477c418b2
Oscar Ballesteros <oscartic@gmail.com>
2021-11-24 22:48:47 -0300

    add Card component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from 'react';
 import UserContext from '../context/Users/UserContext';
+import Card from '../ui/Card';
 
 const BadBank = () => {
     const { getUsers, getProfile, users } = useContext(UserContext);
@@ -11,7 +12,14 @@ const BadBank = () => {
     return (
         <>
             <h3>BadBank Component</h3>
-            {JSON.stringify(users)}
+            <Card 
+                bgcolor="secondary"
+                txtcolor="white"
+                header="BadBank landing Page"
+                title="Welcome to the Bank"
+                text="You can use this bank"
+                body={(<img src="https://ik.imagekit.io/orb/bank_bl8qVd2IE.png?updatedAt=1637803496240" className="img-fluid" alt="responsive image" />)}
+            />
         </>
     )
 };
---
+++ src/ui/Card.js
@@ -0,0 +1,26 @@
+import { Card, Button} from 'react-bootstrap'; 
+const CardBank = ({header, title, text, body, status, bgcolor, txtcolor}) => {
+
+    const classes = () => {
+        const bg = bgcolor ? ` bg-${bgcolor}` : ''; 
+        const txt = txtcolor ? ` text-${txtcolor}` : ' text-white';
+        return `card mb-3 ${bg} ${txt}`;   
+    }
+
+    return (
+        <Card style={{ width: '18rem' }} className={classes()}>
+            <Card.Header>{header}</Card.Header>
+            <Card.Body>
+                <Card.Title>{title && (<h5 className="card-title">{title}</h5>)}</Card.Title>
+                <Card.Text>
+                    {text && (<p className="card-text">{text}</p>)}
+                    {body}
+                    {status && (<div className="createStatus">{status}</div>)}
+                </Card.Text>
+                {/* <Button variant="primary">Go somewhere</Button> */}
+            </Card.Body>
+        </Card>
+    );
+}
+ 
+export default CardBank;
