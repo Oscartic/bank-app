71d269634454b1ef1fd0942dd1a58eac98fcc223
Oscar Ballesteros <oscartic@gmail.com>
2021-11-25 13:55:41 -0300

    add and config create account data

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import UserContext from "../context/Users/UserContext";
+import CardBank from "../ui/Card";
+
 
 const CreateAccount = () => {
-    return(<h1>HOLA DESDE CREATE ACCOUNT </h1>)
+
+    const [show, setShow] = useState(true);
+    const [status, setStatus] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const context = useContext(UserContext)
+
+
+    return(
+        <CardBank
+            bgcolor="primary"
+            txtcolor="white"
+            status={status}
+            header="Create Account"
+            body={show ? (
+                <>
+                    Nane:<br />
+                    <input 
+                        type="input"
+                        className="form-control"
+                        id="name"
+                        placeholder="Enter name"
+                        value={name}
+                        onChange={e => setName(e.currentTarget.value)}
+                    /><br />
+                    Email address:
+                    <input 
+                        type="input"
+                        className="form-control"
+                        id="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={e => setEmail(e.currentTarget.value)}
+                    /><br />
+                </>
+                ) : (
+                <>
+                    <h3>HOLA</h3>
+                </>)}
+        />
+    )
 }
 
 export default CreateAccount;
